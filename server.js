8bda4603e8db943ba8e3cb44cb300f6f20914121
Sam Karp <sfcypret@gmail.com>
2022-10-17 20:34:23 -0400

    server setup completed

--- server.js
+++ server.js
@@ -9,8 +9,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 //routes
+app.use(require('./routes'));
 
 //mongoose database connect
-
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/databasename', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 //log mongo queries being executed. 
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected on localhost: ${PORT}`));
+
