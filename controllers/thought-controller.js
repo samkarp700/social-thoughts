5411b2221c7e34453858a297a645257f73e0274e
Sam Karp <sfcypret@gmail.com>
2022-10-17 20:18:00 -0400

    controller pseudo code

---
+++ controllers/thought-controller.js
@@ -0,0 +1,24 @@
+const {Thought, User } = require('./models');
+
+const thoughtController = {
+    // get all thoughts
+
+
+    //get a single thought by id
+
+
+    //post a new thought -push to user id
+
+
+    //put - update a thought
+
+
+    //delete a thought by id 
+
+
+    //api/thoughts/:thoughtId/reactions
+    //create a reaction 
+
+
+    //delete a reaction by reaction Id
+}
---
+++ controllers/user-controller.js
@@ -0,0 +1,24 @@
+const { User } = require('../models');
+
+ 
+const userController = {
+    //get all users
+
+    //get user by id 
+
+
+    //post a new user 
+
+
+    //update a user by id 
+
+
+    //delete a user by id
+
+    //api/users/:userId/friends/:friendId
+    // new friend 
+
+
+    //delete friend 
+}
+
